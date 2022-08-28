026a36921f81ebf6e033ab28f4139469204ce536
hamid soleymani <hamid.soleymani@gmx.de>
2022-08-29 01:09:22 +0200

    new blog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenueComponent } from './menue/menue.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatSliderModule} from "@angular/material/slider";
+import { NewBlogComponent } from './new-blog/new-blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenueComponent,
+    NewBlogComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatTabsModule,
+    BrowserAnimationsModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
